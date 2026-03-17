a10b48883e21fb4b1f204811c0bb0eb63cf99230
Joseph Westlind <jwestlind@pm.me>
2026-03-17 14:28:05 -0700

    Renamed Ben's bios as bios-bak.s, imported mine as bios.s.

--- bios.s
+++ bios.s
@@ -1,133 +1,138 @@
 .setcpu "65C02"
 .debuginfo
 
-.zeropage
-                .org ZP_START0
-READ_PTR:       .res 1
-WRITE_PTR:      .res 1
+; Adresses for VIA
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+ACR = $600b
+PCR = $600c
+IFR = $600d
+IER = $600e
 
-.segment "INPUT_BUFFER"
-INPUT_BUFFER:   .res $100
+; Adresses for ACIA/UART
+ACIA_DATA   = $5000
+ACIA_STATUS = $5001
+ACIA_CMD    = $5002
+ACIA_CTRL   = $5003
 
-.segment "BIOS"
+lcd_data = $00          ; 1 byte, lcd status read
+LCDCMD = $01            ; 1 byte, lcd command / char
+READ_PTR = $02          ; 1 byte
+WRITE_PTR = $03         ; 1 byte
+LCD1PTR = $04           ; 2 bytes, line 1 address pointer
+LCD2PTR = $06           ; 2 bytes, line 2 address pointer
+IN_BUFFER = $0300       ; 256 Bytes, input buffer
 
-ACIA_DATA       = $5000
-ACIA_STATUS     = $5001
-ACIA_CMD        = $5002
-ACIA_CTRL       = $5003
-PORTA           = $6001
-DDRA            = $6003
+reset:
+    ldx #$ff            ; Initialize stack pointer
+    txs
 
-LOAD:
-                rts
+    cli                 ; Clear interrupt disable flag
 
-SAVE:
-                rts
+    lda #%11111111      ; Set port B data direction
+    sta DDRB
+    lda #%00000111      ; Set port A data direction
+    sta DDRA
 
+    jsr lcd_init
 
-; Input a character from the serial interface.
-; On return, carry flag indicates whether a key was pressed
-; If a key was pressed, the key value will be in the A register
-;
-; Modifies: flags, A
-MONRDKEY:
-CHRIN:
-                phx
-                jsr     BUFFER_SIZE
-                beq     @no_keypressed
-                jsr     READ_BUFFER
-                jsr     CHROUT                  ; echo
-                pha
-                jsr     BUFFER_SIZE
-                cmp     #$B0
-                bcs     @mostly_full
-                lda     #$fe
-                and     PORTA
-                sta     PORTA
-@mostly_full:
-                pla
-                plx
-                sec
-                rts
-@no_keypressed:
-                plx
-                clc
-                rts
+; Setup UART
+uart_config:
+    lda #%00011111      ; 8-N-1, 19200 baud.
+    sta ACIA_CTRL
+    lda #%00001001      ; No parity, no echo, interrupts.
+    sta ACIA_CMD
+    lda #%00011011      ; Begin with escape.
 
+    jsr init_buffer
 
-; Output a character (from the A register) to the serial interface.
-;
-; Modifies: flags
-MONCOUT:
-CHROUT:
-                pha
-                sta     ACIA_DATA
-                lda     #$FF
-@txdelay:       dec
-                bne     @txdelay
-                pla
-                rts
+    jmp WOZMON
 
-; Initialize the circular input buffer
-; Modifies: flags, A
-INIT_BUFFER:
-                lda READ_PTR
-                sta WRITE_PTR
-                lda #$01
-                sta DDRA
-                lda #$fe
-                and PORTA
-                sta PORTA
-                rts
+chrin:
+    jsr buffer_size     ; Get buffer size
+    beq nochar          ; branch if 0
+    cmp #$b4            ; enable cts if less than 180
+    bcc cts_enable
+getchar:
+    jsr read_buffer     ; Get next char from buffer
+    jsr chrout          ; Echo it
+    rts                 ; Return
+nochar:
+    lda #0              ; Return 0 if buffer empty
+    rts
+cts_enable:
+    lda #%11111110      ; Set A0 low (CTS high)
+    and PORTB           ; Don't modify other bits
+    sta PORTB           ; Store
+    jmp getchar
 
-; Write a character (from the A register) to the circular input buffer
-; Modifies: flags, X
-WRITE_BUFFER:
-                ldx WRITE_PTR
-                sta INPUT_BUFFER,x
-                inc WRITE_PTR
-                rts
-
-; Read a character from the circular input buffer and put it in the A register
-; Modifies: flags, A, X
-READ_BUFFER:
-                ldx READ_PTR
-                lda INPUT_BUFFER,x
-                inc READ_PTR
-                rts
-
-; Return (in A) the number of unread bytes in the circular input buffer
-; Modifies: flags, A
-BUFFER_SIZE:
-                lda WRITE_PTR
-                sec
-                sbc READ_PTR
-                rts
+chrout:
+    pha                 ; Save A.
+    sta ACIA_DATA       ; Output character.
+    lda #$FF            ; Initialize delay loop.
+txdelay:
+    dec                 ; Decrement A.
+    bne     txdelay     ; Until A gets to 0.
+    pla                 ; Restore A.
+    rts                 ; Return.
 
 
-; Interrupt request handler
-IRQ_HANDLER:
-                pha
-                phx
-                lda     ACIA_STATUS
-                ; For now, assume the only source of interrupts is incoming data
-                lda     ACIA_DATA
-                jsr     WRITE_BUFFER
-                jsr     BUFFER_SIZE
-                cmp     #$F0
-                bcc     @not_full
-                lda     #$01
-                ora     PORTA
-                sta     PORTA
-@not_full:
-                plx
-                pla
-                rti
+init_buffer:
+    pha
+    lda WRITE_PTR
+    sta READ_PTR
+    pla
+    rts
 
-.include "wozmon.s"
+write_buffer:
+    ldx WRITE_PTR
+    sta IN_BUFFER,x
+    inc WRITE_PTR
+    rts
 
+read_buffer:
+    ldx READ_PTR
+    lda IN_BUFFER,x
+    inc READ_PTR
+    rts
+
+buffer_size:
+    lda WRITE_PTR
+    sec
+    sbc READ_PTR
+    rts
+
+
+.include "code.asm"
+
+.include "lcd.asm"
+
+;interrupt handlers
+nmi:
+irq:
+    pha
+    phx
+    lda ACIA_STATUS
+    bpl exit_irq
+    lda ACIA_DATA       ; Read byte from UART
+    jsr write_buffer
+    jsr buffer_size
+    cmp #$f0
+    bcc exit_irq        ; Branch if not full
+    lda #$1             ; Set B0 High (CTS low)
+    ora PORTB
+    sta PORTB
+exit_irq:
+    plx
+    pla
+    rti
+
+.include "wozmon.asm"
+
+; Reset / interrupt vectors
 .segment "RESETVEC"
-                .word   $0F00           ; NMI vector
-                .word   RESET           ; RESET vector
-                .word   IRQ_HANDLER     ; IRQ vector
-
+    .word nmi
+    .word reset
+    .word irq
